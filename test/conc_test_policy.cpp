d9b1cb36c755b94fd08cbf2abc408ceb5dd13ace
Ben Deane <ben@elbeno.com>
2025-10-14 15:02:15 -0600

    :sparkles: Add test concurrency policy
    Problem:
    - In order to find threading bugs more easily, TSan must have some
      non-determinism to work with.
    
    Solution:
    - Introduce the test concurrency policy, which adds random-duration sleeps on
      entering a critical section.

---
+++ test/conc_test_policy.cpp
@@ -0,0 +1,80 @@
+#include <conc/test.hpp>
+
+#include <catch2/catch_test_macros.hpp>
+
+#include <algorithm>
+#include <array>
+#include <chrono>
+#include <functional>
+#include <iterator>
+#include <random>
+#include <thread>
+
+TEST_CASE("test policy allows 'recursive' critical_sections", "[test_policy]") {
+    auto const value = conc::call_in_critical_section(
+        [] { return conc::call_in_critical_section([] { return 1; }); });
+    CHECK(value == 1);
+}
+
+namespace {
+struct rng_CS;
+struct count_CS;
+
+auto get_rng() -> auto & {
+    std::array<int, std::mt19937::state_size> seed_data;
+    std::random_device r;
+    std::generate_n(seed_data.data(), seed_data.size(), std::ref(r));
+    std::seed_seq seq(std::begin(seed_data), std::end(seed_data));
+    static std::mt19937 rng(seq);
+    return rng;
+}
+} // namespace
+
+TEST_CASE("test policy works", "[test_policy]") {
+    conc::test_policy::reset_counts();
+    constexpr auto N = 10u;
+    auto &rng = get_rng();
+    auto count = 0;
+    auto dis = std::uniform_int_distribution{1, 10};
+
+    std::array<std::thread, N> threads{};
+    for (auto i = 0u; i < N; ++i) {
+        threads[i] = std::thread{[&] {
+            auto const d = conc::call_in_critical_section(
+                [&] { return std::chrono::milliseconds{dis(rng)}; });
+            std::this_thread::sleep_for(d);
+            conc::call_in_critical_section([&] { ++count; });
+        }};
+    }
+    for (auto i = 0u; i < N; ++i) {
+        threads[i].join();
+    }
+
+    CHECK(count == N);
+    CHECK(conc::test_policy::lock_count == N * 2);
+    CHECK(conc::test_policy::unlock_count == N * 2);
+}
+
+TEST_CASE("test policy allows different-ID critical_sections",
+          "[test_policy]") {
+    auto &rng = get_rng();
+    auto count = 0;
+    auto dis = std::uniform_int_distribution{1, 10};
+
+    auto t1 = std::thread{[&] {
+        auto const d = conc::call_in_critical_section<rng_CS>(
+            [&] { return std::chrono::milliseconds{dis(rng)}; });
+        std::this_thread::sleep_for(d);
+        conc::call_in_critical_section<count_CS>([&] { ++count; });
+    }};
+    auto t2 = std::thread{[&] {
+        auto const d = conc::call_in_critical_section<rng_CS>(
+            [&] { return std::chrono::milliseconds{dis(rng)}; });
+        std::this_thread::sleep_for(d);
+        conc::call_in_critical_section<count_CS>([&] { ++count; });
+    }};
+    t1.join();
+    t2.join();
+
+    CHECK(count == 2);
+}
